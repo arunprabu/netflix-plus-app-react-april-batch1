e8611fa1232ddf6ac807b8a0758afbba01a2b90f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-06-26 11:00:53 +0530

    updated

--- netflix-plus-app/src/pages/HooksDemoPage/TodosVariant2/TodosVariant2.js
+++ netflix-plus-app/src/pages/HooksDemoPage/TodosVariant2/TodosVariant2.js
@@ -1,10 +1,31 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useReducer, useRef } from 'react'
+import todoReducer from '../../../reducers/todoReducer';
 
 const TodosVariant2 = () => {
   const todoInput = useRef('');
 
+  const [todoList, todoDispatch] = useReducer(todoReducer);
+  console.log(todoList); // undefined -- get the state data from reducer. How?
+  console.log(todoDispatch); // dispatcher fn -- part of react core
+  // dispatcher fn must be called with an action obj to get state data from the reducer
+
+  useEffect(() => {
+    todoDispatch({
+      type: 'LIST_TODOS'
+    })
+  }, []);
+
   const handleAddTodo = () => {
     console.log('will add todo');
+    console.log(todoInput.current.value);
+    todoDispatch({
+      type: 'ADD_TODO',
+      payload: {
+        id: Math.floor(Math.random() * 100000),
+        title: todoInput.current.value,
+        isCompleted: false
+      }
+    });
   }
 
   return (
@@ -27,7 +48,11 @@ const TodosVariant2 = () => {
         <div className="col-md-4">
           <div className="card">
             <ul className="list-group list-group-flush">
-              <li className="list-group-item">first todo</li>
+              {
+                todoList?.map((todo) => {
+                  return <li key={todo.id} className="list-group-item">{todo.title}</li>;
+                })
+              }
             </ul>
           </div>
         </div>
---
+++ netflix-plus-app/src/reducers/todoReducer.js
@@ -0,0 +1,25 @@
+// What's a reducer?
+/*
+ * reducer is a fn
+ * that takes in state and action
+ * it must return new state based on the type of action dispatched.
+ * action is an object -- that must have type property and an optional payload
+ */
+
+const todoReducer = (state = [], action) => {
+  console.log('called todoReducer');
+  // it must return new state based on the type of action dispatched
+
+  switch (action.type) {
+  case 'LIST_TODOS':
+    return state;
+
+  case 'ADD_TODO':
+    return [...state, action.payload];
+
+  default:
+    return state;
+  }
+};
+
+export default todoReducer;
